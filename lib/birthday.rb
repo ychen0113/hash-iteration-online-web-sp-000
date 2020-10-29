20d8ddaa6b9e27776cd29be24f25b5e9609713e2
Yiliang Chen <yiliangchen0113@gmail.com>
2020-10-29 20:24:57 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,7 +12,7 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday(birthday_kids)
-  birthday_kids.each do |kid_name, age|
+  birthday_kids.each do |kids_name, age|
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!" if age <= 12
   end
 end
